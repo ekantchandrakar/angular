5404dc798f50b54bc89da6fdaf0cdc283b149a34
Ekant Chandrakar <ekantchandrakar07@gmail.com>
2024-02-18 17:09:24 +0530

    Property Binding4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
-  title = 'AngIntro';
-  parentMessage: string = 'Message coming from parent component';
-  messageFromChild: string;
-  fromChildOutput: string;
-
-  @ViewChild(PostComponent) childComponent: any;
-
-  constructor() {
-    console.log(this.childComponent);
-  }
-
-  ngAfterViewInit(): void {
-    console.log(this.childComponent);
-    this.messageFromChild = this.childComponent.childMessage;
-  }
-
-  receiveMessage($event: any) {
-    this.fromChildOutput = $event;
-  }
+export class AppComponent {
+  messsage: string = 'Message from the TypeScript component file';
+  imgUrl: string =
+    'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg';
 }
