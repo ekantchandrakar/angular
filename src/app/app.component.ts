4efec53900eb70e678ceee0a6b271fc1d1489539
Ekant Chandrakar <ekantchandrakar07@gmail.com>
2024-02-24 17:54:44 +0530

    Event Binding and filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  messsage: string = 'Message from the TypeScript component file';
-  imgUrl: string =
-    'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg';
+  isTrue: boolean = false;
+
+  buttonClick(): void {
+    console.log('Button Click Event Worked');
+  }
+
+  onKeyUp(): void {
+    console.log('Enter key is pressed');
+  }
 }
